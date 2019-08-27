d475d0c7f8aa05eb41ff27ab8120ff39c57e33e1
Matt Paine <matt@mattsoftware.com>
2019-08-27 11:54:33 +1000

    Added sns event notification

--- services/sns.js
+++ services/sns.js
@@ -7,3 +7,37 @@ module.exports.publish = message => {
     };
 };
 
+// sam local generate-event sns notification
+module.exports.event_notification = notifications => {
+    return {
+        "Records": notifications.map(notification => ({
+            "EventSource": "aws:sns",
+            "EventVersion": "1.0",
+            "EventSubscriptionArn": "arn:aws:sns:us-east-1::ExampleTopic",
+            "Sns": {
+                "Type": "Notification",
+                "MessageId": "95df01b4-ee98-5cb9-9903-4c221d41eb5e",
+                "TopicArn": "arn:aws:sns:us-east-1:123456789012:ExampleTopic",
+                "Subject": "example subject",
+                "Message": "example message",
+                "Timestamp": "1970-01-01T00:00:00.000Z",
+                "SignatureVersion": "1",
+                "Signature": "EXAMPLE",
+                "SigningCertUrl": "EXAMPLE",
+                "UnsubscribeUrl": "EXAMPLE",
+                "MessageAttributes": {
+                    "Test": {
+                        "Type": "String",
+                        "Value": "TestString"
+                    },
+                    "TestBinary": {
+                        "Type": "Binary",
+                        "Value": "TestBinary"
+                    }
+                },
+                ...notification,
+            },
+        }))
+    };
+};
+
